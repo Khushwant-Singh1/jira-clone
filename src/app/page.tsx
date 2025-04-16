b704359c7a1162b8ae17f74846a6af7394cba82f
Khushwant-Singh1 <khushwantzx@gmail.com>
2025-04-16 23:08:44 +0530

    fix: correct start script in package.json and improve component imports and styling in UI components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
-    
     <div>
-
+      <Input />
       <Button size={"sm"}> Click me</Button>
-      
-      </div>
+    </div>
   );
 }
